27e2baee80642c0cb6202f77dd37b835411d62d8
juzzrespecter <danisarp@gmail.com>
2023-12-18 03:42:20 +0100

    Planteamiento servicios bonus

--- srcs/requirements/mariadb/conf/mariadb-entrypoint.sh
+++ srcs/requirements/mariadb/conf/mariadb-entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
+# Setup ENV variables 
+
+sed "/MYSQL_ROOT_PASSWD/${MYSQL_ROOT_PASSWD}/" /tmp/mariadb-setup.sql
+sed "/MYSQL_DB/${MYSQL_DB}/" /tmp/mariadb-setup.sql
+sed "/MYSQL_USER/${MYSQL_USER}/" /tmp/mariadb-setup.sql
+sed "/MYSQL_USER_PASSWD/${MYSQL_USER_PASSWD}/" /tmp/mariadb-setup.sql
+
 # First time setup for ROOT & WP database config.
-openrc 
+
+openrc && \
+    mysql < /tmp/mariadb-setup.sh
+
+# Set WP database tables
+
+# Run mysqld in foreground as root
+
+mysql_safe
